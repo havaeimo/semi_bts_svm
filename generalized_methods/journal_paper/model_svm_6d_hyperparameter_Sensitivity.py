1b41a0ea2fc6634d52e24be6c137585698594617
Mohammad Havaei <mohammad.havaei@gmail.com>
2014-10-07 01:01:43 -0400

    updated scripts svm_sensitivity.py and model_svm_6d_hyperparameter_Sensitivity.py

--- generalized_methods/journal_paper/model_svm_6d_hyperparameter_Sensitivity.py
+++ generalized_methods/journal_paper/model_svm_6d_hyperparameter_Sensitivity.py
@@ -11,98 +11,50 @@ from mlpython.learners.third_party.libsvm.classification import SVMClassifier
 import compute_statistics
 import time
 import data_utils
-#import ipdb
 
-# make a dictionary of the selected brains with their best c and gamma parameters. 
-brain_list = {'HG_0002': [1,50], 'HG_0001': [1,0.01], 'HG_0003': [1,1], 'HG_0010': [1,50], 'HG_0008': [1,5], 'HG_0012': [50,50], 'HG_0011': [1,5], 'HG_0022': [1,5], 'HG_0025': [1,50], 'HG_0027': [1,10], 'LG_0008': [1,200], 'LG_0001': [1,50], 'LG_0006': [1,1], 'LG_0015': [100,200], 'HG_0014': [1,5]}
-sys.argv.pop(0); # Remove first argument
-
-# Check if every option(s) from parent's script are here.
-
-arg_length = len(sys.argv)
-if arg_length not in [3,5,6]:
-    print len(sys.argv)
-    print "Usage: python model_libsvm.py mlpython_dataset_directory dataset_name output_folder [train_filename test_filename [background_filename]]"
-    print "Filenames for the train/test/background files are necessary on the first use, they are not needed later on, as trainset/validset/testset files are created"
-    print "Ex.: python model_libsvm.py brains_data/Brats-2_training/ HG_0001 . interaction.txt allpoints.txt background.txt"
-    sys.exit()
-
-
-# Get arguments
-dataset_directory = sys.argv[0]
-dataset_name = sys.argv[1]
-output_folder = sys.argv[2]
-train_filename = None
-test_filename = None
-background_filename = None
-
-if arg_length >= 4:
-    train_filename = sys.argv[3]
-    test_filename = sys.argv[4]
-    
-    if arg_length == 6:
-        background_filename = sys.argv[5]
-    
-
-spatial_dimensions = 1
-#use_weights = True if sys.argv[1] == 'True' else False
-use_weights = False
-input_size = 6 # Do we want this as command-line parameter?
-
-# Set the constructor
-str_ParamOption = ""
-str_ParamOptionValue = ""
-
-dataset_dir = None
-if dataset_dir is None:
-    # Try to find dataset in MLPYTHON_DATASET_REPO
+def load_data(dataset_directory , dataset_name):
+    print "Loading datasets ..."
     import os
     repo = os.environ.get('MLPYTHON_DATASET_REPO')
     if repo is None:
         raise ValueError('environment variable MLPYTHON_DATASET_REPO is not defined')
-    dataset_dir = os.path.join(os.environ.get('MLPYTHON_DATASET_REPO') + '/' + dataset_directory, dataset_name)
+    dataset_dir = os.path.join(os.environ.get('MLPYTHON_DATASET_REPO') + '/' + dataset_directory, dataset_name)    
+    
+    input_size = 6 
+    spatial_dimensions = 1
+    all_data = data_utils.load_data(dir_path=dataset_dir, input_size=input_size, train_filename=None, test_filename=None, background_filename=None,load_to_memory=False)
 
+    train_data, train_metadata = all_data['train']
+    valid_data, valid_metadata = all_data['valid']
+    finaltrain_data, finaltrain_metadata = all_data['finaltrain']
+    test_data, test_metadata = all_data['test']
+   
 
-# Load data
-start_time = time.clock()
+    def reduce_dimensionality(mlproblem_data, mlproblem_metadata):
+        mlproblem_metadata['input_size'] = 3  # we need to change the input size from 6 to 3. 
+        return [mlproblem_data[0][:3] , mlproblem_data[1]]
 
-all_data = data_utils.load_data(dir_path=dataset_dir, input_size=input_size, train_filename=train_filename, test_filename=test_filename, background_filename=background_filename,load_to_memory=False)
+    if spatial_dimensions ==1:      
+        import mlpython.mlproblems.classification as mlpb
+        trainset = mlpb.ClassificationProblem(train_data, train_metadata)
+        validset = trainset.apply_on(valid_data,valid_metadata)
+        finaltrainset = trainset.apply_on(finaltrain_data,finaltrain_metadata)
+        testset = trainset.apply_on(test_data,test_metadata)
 
-train_data, train_metadata = all_data['train']
-valid_data, valid_metadata = all_data['valid']
-finaltrain_data, finaltrain_metadata = all_data['finaltrain']
-test_data, test_metadata = all_data['test']
+    elif spatial_dimensions ==0:
+        import mlpython.mlproblems.generic as mlpg
+        trainset = mlpg.PreprocessedProblem(data = train_data , metadata = train_metadata , preprocess = reduce_dimensionality)
+        validset = trainset.apply_on(valid_data, valid_metadata)
+        testset = trainset.apply_on(test_data, test_metadata)
+        finaltrainset = trainset.apply_on(finaltrain_data, finaltrain_metadata)
+        import mlpython.mlproblems.classification as mlpb
+        trainset = mlpb.ClassificationProblem(trainset, trainset.metadata)
+        validset = trainset.apply_on(validset,validset.metadata)
+        finaltrainset = trainset.apply_on(finaltrainset,finaltrainset.metadata)
+        testset = trainset.apply_on(testset,testset.metadata)
 
-def reduce_dimensionality(mlproblem_data, mlproblem_metadata):
-    mlproblem_metadata['input_size'] = 3  # we need to change the input size from 6 to 3. 
-    return [mlproblem_data[0][:3] , mlproblem_data[1]]
+    return {'trainset':trainset, 'validset':validset}    
 
-if spatial_dimensions ==1:      
-    import mlpython.mlproblems.classification as mlpb
-    trainset = mlpb.ClassificationProblem(train_data, train_metadata)
-    validset = trainset.apply_on(valid_data,valid_metadata)
-    finaltrainset = trainset.apply_on(finaltrain_data,finaltrain_metadata)
-    testset = trainset.apply_on(test_data,test_metadata)
-
-elif spatial_dimensions ==0:
-    import mlpython.mlproblems.generic as mlpg
-    trainset = mlpg.PreprocessedProblem(data = train_data , metadata = train_metadata , preprocess = reduce_dimensionality)
-    validset = trainset.apply_on(valid_data, valid_metadata)
-    testset = trainset.apply_on(test_data, test_metadata)
-    finaltrainset = trainset.apply_on(finaltrain_data, finaltrain_metadata)
-    import mlpython.mlproblems.classification as mlpb
-    trainset = mlpb.ClassificationProblem(trainset, trainset.metadata)
-    validset = trainset.apply_on(validset,validset.metadata)
-    finaltrainset = trainset.apply_on(finaltrainset,finaltrainset.metadata)
-    testset = trainset.apply_on(testset,testset.metadata)
-'''
-for d,i in enumerate(testset):
-    print i
-    if d>10:
-        break
-
-ipdb.set_trace() 
-'''
 def compute_error_mean_and_sterror(costs):
     classif_errors = np.hstack(costs)
     classif_mean = classif_errors.mean()
@@ -110,71 +62,15 @@ def compute_error_mean_and_sterror(costs):
 
     return classif_mean, classif_sterror
 
-
-print "Setting hyperparameters gridsearch..."
-best_hyperparams = None
-best_val_error = np.inf
-
-# SVM model documentation
-"""
-    -t kernel_type : set type of kernel function (default 2)
-        0 -- linear: u'*v
-        1 -- polynomial: (gamma*u'*v + coef0)^degree
-        2 -- radial basis function: exp(-gamma*|u-v|^2)
-        3 -- sigmoid: tanh(gamma*u'*v + coef0)
-    -d degree : set degree in kernel function (default 3)
-    -g gamma : set gamma in kernel function (default 1/num_features)
-    -r coef0 : set coef0 in kernel function (default 0)
-    -c cost : set the parameter C of C-SVC, epsilon-SVR, and nu-SVR (default 1)
-    -b probability_estimates: whether to train a SVC or SVR model for probability estimates, 0 or 1 (default 0)
-"""
-output_probabilities = True # Or False!
-kernels = ['rbf','sigmoid']
-degrees = [1,2,3,4,5,7,10,15]
-gammas = [0.01,0.1,1,5,10,50,100,150,200,500,1000]
-coef0s = [-10,-1,-0.1,-0.01,0,0.001,0.01,0.1,1,2,5,10,20]
-Cs = [1,10,50,100,500,1000,1500,2000,2500,3000,5000]
-'''
-kernels = ['rbf']
-degrees = [1]
-gammas = [0.01]
-coef0s = [-10]
-Cs = [1]
-'''
-
-
-hyperparams_grid = []
-for C in Cs:
-    # Linear kernel parameters
-    hyperparams_grid.append(['linear', 3, 1, 0, C])
-
-    # Polynomial kernel parameters
-    """
-    for gamma in gammas:
-        for coef0 in coef0s:
-            for degree in degrees:
-                hyperparams_grid.append(['polynomial', degree, gamma, coef0, C])
-    """
-    # Rbf kernel parameters
-    """
-    for gamma in gammas:
-        hyperparams_grid.append(['rbf', 3, gamma, 0, C])
-    """    
-    #Sigmoid kernel parameters
-    """
-    for gamma in gammas:
-        for coef0 in coef0s:
-            hyperparams_grid.append(['sigmoid', 3, gamma, coef0, C])
-    """        
-if use_weights:
-    label_weights = finaltrainset.metadata['label_weights']
-else:
-    label_weights = None
-    
-output_probabilities = True # Or False!
-
-print "Pretraining..."
-for params in hyperparams_grid:
+def svm_model(dataset_directory, dataset_name, params, datasets):
+    start_time = time.clock()
+    use_weights = False    
+    if use_weights:
+        label_weights = finaltrainset.metadata['label_weights']
+    else:
+        label_weights = None
+        
+    output_probabilities = True # Or False!
     try:
         # Create SVMClassifier with hyper-parameters
         svm = SVMClassifier(shrinking=True, kernel=params[0],degree=params[1],gamma=params[2],coef0=params[3],C=params[4],label_weights=label_weights, output_probabilities=output_probabilities)
@@ -182,61 +78,26 @@ for params in hyperparams_grid:
         print "Error while instantiating SVMClassifier (required hyper-parameters are probably missing)"
         print inst
         sys.exit()
-
+    trainset = datasets['trainset']
     svm.train(trainset)
+    validset = datasets['validset']
     outputs, costs = svm.test(validset)
     
     errors = compute_error_mean_and_sterror(costs)
     error = errors[0]
+    end_time = time.clock()
+    processing_time = end_time - start_time
+    return [error , processing_time]    
+
     
-    if error < best_val_error:
-        best_val_error = error
-        best_hyperparams = params
-
-print
-print 'Classification error on valid set : ' + str(best_val_error)
-print
-
-print "Training..."
-# Train SVM with best hyperparams on train + validset
-#gamma = 5.0
-#C = 1
-#best_hyperparams = ['rbf',3,gamma,0,C]
-best_svm = SVMClassifier(shrinking=True, kernel=best_hyperparams[0],degree=best_hyperparams[1],gamma=best_hyperparams[2],coef0=best_hyperparams[3],C=best_hyperparams[4],label_weights=label_weights, output_probabilities=output_probabilities)
-best_svm.train(finaltrainset)
-
-print 'Testing...'
-outputs, costs = best_svm.test(testset)
-
-errors = compute_error_mean_and_sterror(costs)
-error = errors[0]
-
-print
-print 'Classification error on test set : ' + str(error)
-print "****************************************"
-
-
-# Evaluation (compute_statistics.py)
-id_to_class = {}
-for label, id in testset.class_to_id.iteritems():
-    id_to_class[id] = label
-    
-lbl = np.array([int(data[1]) for data in test_data]) # Ground truth
-auto_lbl = np.array([int(id_to_class[output[0]]) for output in outputs]) # Predicted labels
-"""
-WARNING!
-If using probability distributions, we need to remap the ids to the right class (for all outputs)
-"""
-
-
+   
+'''
 # Write outputs to disk if result file does not already exist
 results_path = output_folder + 'libsvm_results/'
 if not os.path.exists(results_path):
     os.makedirs(results_path)
-if use_weights:
-    output_file = results_path + dataset_name + '_weightedlabels_output_txt'
-else:
-    output_file = results_path + dataset_name + '_libsvm_output.txt'
+
+output_file = results_path + dataset_name + '_libsvm_output.txt'
 if not os.path.exists(output_file):
     with open(output_file,'w') as f:
         for output, data in zip(outputs, test_data):
@@ -250,7 +111,8 @@ if not os.path.exists(output_file):
             f.write(line)
 
 # Compute processing time
-end_time = time.clock()
+
+
 print "Processing time : " + str(float(end_time - start_time) / 60) + ' minutes'
 print
 
@@ -320,6 +182,6 @@ else:
     with open(results_path + results_file,'a') as f:
         f.write(result)
         
-
+'''
 
 
---
+++ generalized_methods/journal_paper/svm_sensitivity.py
@@ -0,0 +1,90 @@
+import numpy as np
+import os
+import sys
+import fcntl
+import copy
+#import pdb
+sys.path.append('/home/local/USHERBROOKE/havm2701/git.repos/semi_bts_svm/semi_bts_svm/generalized_methods/')
+from string import Template
+import mlpython.datasets.store as dataset_store
+from mlpython.learners.third_party.libsvm.classification import SVMClassifier
+import compute_statistics
+import time
+import data_utils
+from model_svm_6d_hyperparameter_Sensitivity import svm_model as svm_model
+from model_svm_6d_hyperparameter_Sensitivity import load_data as load_data
+#import ipdb
+
+# make a dictionary of the selected brains with their best c and gamma parameters. 
+brain_list = {'HG_0002': [1,50], 'HG_0001': [1,0.01], 'HG_0003': [1,1], 'HG_0010': [1,50], 'HG_0008': [1,5], 'HG_0012': [50,50], 'HG_0011': [1,5], 'HG_0022': [1,5], 'HG_0025': [1,50], 'HG_0027': [1,10], 'LG_0008': [1,200], 'LG_0001': [1,50], 'LG_0006': [1,1], 'LG_0015': [100,200], 'HG_0014': [1,5]}
+sys.argv.pop(0); # Remove first argument
+
+# Get arguments
+dataset_directory = sys.argv[0]
+#dataset_name = sys.argv[1]
+output_folder = sys.argv[1]
+
+results_path = output_folder + '/libsvm_results/'
+if not os.path.exists(results_path):
+    os.makedirs(results_path)
+
+gammas = [0.001,0.005, 0.01,0.05,0.1,0.5,1,5,10,50,100,150,200,300,400,500,1000]
+Cs = [1,5,10,25,50,75,100,150,200,250,300,400,500,750,1000,1250,1500]
+
+# measure the sensitivity of gamma for the selected brains and save the text file
+
+brain_names = brain_list.keys()
+results_file_c = 'libsvm_measures_C.txt'
+results_file_g = 'libsvm_measures_gamma.txt'
+
+for brain in brain_names:
+    resultg1, resultg2 = '' ,''
+    datasets = load_data(dataset_directory , brain)
+    C = brain_list[brain][0]
+    brain_str = brain + '\t' + 'C=' + str(C) + ' \n'
+    
+    for gamma in gammas:
+        paramsg = ['rbf', 3, gamma, 0, C]
+        errorg , processed_timeg = svm_model(dataset_directory, brain, paramsg, datasets)
+        
+        resultg1 += "%.4f" % errorg + '\t'
+        resultg2 += "%.4f" % processed_timeg + '\t'
+    
+    resultg1 += '\n'
+    resultg2 += '\n'
+     
+    if not os.path.exists(results_path + results_file_g):
+          with open(results_path + results_file_g,'w') as g:
+            g.write(brain_str)
+            g.write(resultg1)
+            g.write(resultg2)
+    else:
+          with open(results_path + results_file_g,'a') as g:
+              g.write(resultg1)
+              g.write(resultg2) 
+
+
+    resultc1, resultc2 = '' ,''           
+    gamma = brain_list[brain][1]
+    brain_str = brain + '\t' + 'gamma=' + str(gamma) + ' \n'
+    
+    for C in Cs:
+        paramsc = ['rbf', 3, gamma, 0, C]
+        errorc , processed_timec = svm_model(dataset_directory, brain, paramsc, datasets)
+        
+        resultc1 += "%.7f" % errorc + '\t'
+        resultc2 += "%.4f" % processed_timec + '\t'
+    
+    resultc1 += '\n'
+    resultc2 += '\n'
+     
+    if not os.path.exists(results_path + results_file_c):
+          with open(results_path + results_file_c,'w') as c:
+            c.write(brain_str)
+            c.write(resultc1)
+            c.write(resultc2)
+    else:
+          with open(results_path + results_file_c,'a') as c:
+              c.write(resultc1)
+              c.write(resultc2) 
+
