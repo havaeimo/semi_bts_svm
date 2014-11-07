d7b3f0afc51de998d84244b337f1fdf2be5feb7a
Mohammad Havaei <seyed.mohammad.havaei@usherbrooke.ca>
2014-11-07 16:53:34 -0500

    working version of multiprocess

--- generalized_methods/journal_paper/multiprocessing/svm_sensitivity.py
+++ generalized_methods/journal_paper/multiprocessing/svm_sensitivity.py
@@ -3,7 +3,7 @@ import os
 import sys
 import fcntl
 import copy
-import pdb
+
 from multiprocessing import Pool
 
 sys.path.append('/home/local/USHERBROOKE/havm2701/git.repos/semi_bts_svm/semi_bts_svm/generalized_methods/')
@@ -18,7 +18,7 @@ import mlpython.mlproblems.generic as mlpg
 from mlpython.mlproblems.generic import SubsetProblem
 # make a dictionary of the selected brains with their best c and gamma parameters. 
 #brain_list = {'HG_0002': [1,50], 'HG_0001': [1,0.01], 'HG_0003': [1,1], 'HG_0010': [1,50], 'HG_0008': [1,5], 'HG_0012': [50,50], 'HG_0011': [1,5], 'HG_0022': [1,5], 'HG_0025': [1,50], 'HG_0027': [1,10], 'LG_0008': [1,200], 'LG_0001': [1,50], 'LG_0006': [1,1], 'LG_0015': [100,200], 'HG_0014': [1,5]}
-global svm
+
 
 
 def load_data(dataset_directory , dataset_name):
@@ -114,14 +114,16 @@ def svm_model(dataset_directory, dataset_name, params, datasets):
 
 
 
-def svm_train(datasets,params):
+def svm_train_test(input_arg):
+
 
     label_weights = None
-        
+    best = input_arg[1]
+    testset = input_arg[0]    
     output_probabilities = True # Or False!
     try:
         # Create SVMClassifier with hyper-parameters
-        svm_best = SVMClassifier(shrinking=True, kernel=params[0],degree=params[1],gamma=params[2],coef0=params[3],C=params[4],label_weights=label_weights, output_probabilities=output_probabilities)
+        svm_best = SVMClassifier(shrinking=True, kernel=best[0],degree=best[1],gamma=best[2],coef0=best[3],C=best[4],label_weights=label_weights, output_probabilities=output_probabilities)
     except Exception as inst:
         print "Error while instantiating SVMClassifier (required hyper-parameters are probably missing)"
         print inst
@@ -130,7 +132,8 @@ def svm_train(datasets,params):
 
     finaltrainset = datasets['finaltrainset']
     svm_best.train(finaltrainset)
-    return svm_best
+    outputs, costs = svm_best.test(testset)
+    return (outputs,costs)
     
 
 
@@ -161,7 +164,7 @@ def parrallelize_testset(testset,cores):
 
 def reduce_dimensionality(mlproblem_data, mlproblem_metadata):
     mlproblem_metadata['input_size'] = 6
-    pdb.set_trace()  # we need to change the input size from 6 to 3. 
+      # we need to change the input size from 6 to 3. 
     return [mlproblem_data[0][:3] , mlproblem_data[1]]
 
 
@@ -195,8 +198,8 @@ if __name__ == '__main__':
     	core_params.append(hyperparams_grid[index*len_process:index*len_process+len_process])
 
 
-    multiprocessing_start_time = time.clock()
-    pdb.set_trace()
+    multiprocessing_start_time = time.time()
+
     print 'finidng hyper-parameters...'
     results = pool.map(find_best_model, (core_params[0],core_params[1],core_params[2],core_params[3],core_params[4],core_params[5],core_params[6],core_params[7]))
     er = [t[1] for t in results]
@@ -204,7 +207,7 @@ if __name__ == '__main__':
     index_best = er.argmin()
     best_params = results[index_best][0]
 
-    multiprocessing_end_time = time.clock()
+    multiprocessing_end_time = time.time()
 
     multiprocessing_processing_time = multiprocessing_end_time - multiprocessing_start_time
     print 'finidng hyper-parameters took ' +str(multiprocessing_processing_time)+' seconds'
@@ -215,23 +218,20 @@ if __name__ == '__main__':
     singlethread_processing_time = singlethread_end_time - singlethread_start_time
     #dice_g , processed_timeg = svm_model(dataset_directory, brain, params, datasets)
     '''
-    pdb.set_trace()
     print 'training...'
-    t1 = time.clock()
-    svm = svm_train(datasets,best_params)
-
-    t2 = time.clock()
-    print 'training took ' + str((t2-t1)) 	
+	
     testset = datasets['testset']
     len_testset_core = np.int(np.ceil(len(testset)/(cores*1.0)))
+
     core_testset = []
     for index in range(cores):
-    	core_testset.append([t for t in SubsetProblem(testset,subset=set(range(index*len_process,index*len_process+len_process)))])
+        core_testset.append([[t for t in SubsetProblem(testset,subset=set(range(index*len_process,index*len_process+len_process)))],best_params])
     print 'testing...'
-    pdb.set_trace()
-    t1 = time.clock()	
-    predictions = pool.map(svm_test, (core_testset[0],core_testset[1],core_testset[2],core_testset[3],core_testset[4],core_testset[5],core_testset[6],core_testset[7]))
-    t2 = time.clock()
+    
+    t1 = time.time()	
+    pool2 = Pool(processes=cores)
+    predictions = pool2.map(svm_train_test, (core_testset[0],core_testset[1],core_testset[2],core_testset[3],core_testset[4],core_testset[5],core_testset[6],core_testset[7]))
+    t2 = time.time()
     print 'testing took ' + str((t2-t1)) 
     #core_testset = parrallelize_testset(testset,cores)
     #pdb.set_trace()
